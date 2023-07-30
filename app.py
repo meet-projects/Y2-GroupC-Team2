7d4b70f519343cea75cfc15cb804ff03603f25bc
Yoav-Koursh <yoav.koursh@gmail.com>
2023-07-30 17:40:30 +0300

    even more work

--- app.py
+++ app.py
@@ -1,8 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
-from flask_mail imprt Mail, Message
+from flask_mail import Mail, Message
 import random 
+
+
+config = {
+
+  'apiKey': "AIzaSyDVj6QQxX6ZotMxluwEMC58eST9HShAIUE",
+
+  'authDomain': "final2-e2f3e.firebaseapp.com",
+
+  'databaseURL': "https://final2-e2f3e-default-rtdb.europe-west1.firebasedatabase.app",
+
+  'projectId': "final2-e2f3e",
+
+  'storageBucket': "final2-e2f3e.appspot.com",
+
+  'messagingSenderId': "894344417219",
+
+  'appId': "1:894344417219:web:49d852ac015d452f717776",
+
+'databaseURL':"https://final2-e2f3e-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+firebase=pyrebase.initialize_app(config)
+db=firebase.database()
+app = Flask(__name__, template_folder='templates', static_folder='static')
+app.config['SECRET_KEY'] = 'super-secret-key'
+
 mail=Mail(app)
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT']= 465
@@ -10,31 +36,9 @@ app.config['MAIL_USERNAME']= 'poad7498@gmail.com'
 app.config['MAIL_PASSWORD']= 'doaayoav'
 app.config['MAIL_USE_SSL']= True
 mail=Mail(app)
+
 auth= firebase.auth()
 
-const firebaseConfig = {
-
-  apiKey: "AIzaSyDVj6QQxX6ZotMxluwEMC58eST9HShAIUE",
-
-  authDomain: "final2-e2f3e.firebaseapp.com",
-
-  databaseURL: "https://final2-e2f3e-default-rtdb.europe-west1.firebasedatabase.app",
-
-  projectId: "final2-e2f3e",
-
-  storageBucket: "final2-e2f3e.appspot.com",
-
-  messagingSenderId: "894344417219",
-
-  appId: "1:894344417219:web:49d852ac015d452f717776"
-
-databaseURL:"https://final2-e2f3e-default-rtdb.europe-west1.firebasedatabase.app/"
-};
-
-firebase=pyrebase.initialize_app(config)
-db=firebase.database()
-app = Flask(__name__, template_folder='templates', static_folder='static')
-app.config['SECRET_KEY'] = 'super-secret-key'
 
 arabictext=[]
 hebrewtext=[]
@@ -46,8 +50,9 @@ def about():
         email=request.form['email']
         location= request.form['location']
         question=request.form['question']
-        question={'name':name, 'email':email, 'location':locatoin, 'question':question}
+        question={'name':name, 'email':email, 'location':location, 'question':question}
         db.child('questions').push(question)
+    return render_template('about.html')
 @app.route('/admin')
 def adminlogin():
     if request.method=='POST':
@@ -58,7 +63,7 @@ def adminlogin():
             return redirect(url_for('admin'))
     else:
         return render_template('adminlogin.html')
-@app.route ('/Answers', methods-['POST', 'GET'])
+@app.route ('/Answers', methods=['POST', 'GET'])
 def awnser():
     if request.method=='POST':
         msg= Message('Awnser for question about out charity "shekulutov"', sender="poad7498@gmail.com", recipients= login_session['recipientemail'] )
