9e3c2865fd20cbeb8bb8cbff3f32f32f05ad8514
Yoav-Koursh <yoav.koursh@gmail.com>
2023-07-30 16:22:50 +0300

    work
    idk

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from flask import session as login_session
 import pyrebase
 from flask_mail imprt Mail, Message
+import random 
 mail=Mail(app)
 app.config['MAIL_SERVER']= 'smtp.gmail.com'
 app.config['MAIL_PORT']= 465
@@ -9,7 +10,7 @@ app.config['MAIL_USERNAME']= 'poad7498@gmail.com'
 app.config['MAIL_PASSWORD']= 'doaayoav'
 app.config['MAIL_USE_SSL']= True
 mail=Mail(app)
-
+auth= firebase.auth()
 
 const firebaseConfig = {
 
@@ -40,17 +41,41 @@ hebrewtext=[]
 englishtext=[]
 @app.route('/' , methods= ('POST', "get"))
 def about():
-    if request.method="POST":
+    if request.method=="POST":
         name=request.form['name']
         email=request.form['email']
         location= request.form['location']
         question=request.form['question']
         question={'name':name, 'email':email, 'location':locatoin, 'question':question}
-        db.child('questoins').push(question)
+        db.child('questions').push(question)
 @app.route('/admin')
-def login_session():
+def adminlogin():
+    if request.method=='POST':
+        username= request.form[ 'username']
+        password = request.form ['password']
+        if db.child('auth').get().val()[username]==password:
+            login_session['user']=auth.sign_in_with_email_and_password(username, password)
+            return redirect(url_for('admin'))
+    else:
+        return render_template('adminlogin.html')
+@app.route ('/Answers', methods-['POST', 'GET'])
+def awnser():
+    if request.method=='POST':
+        msg= Message('Awnser for question about out charity "shekulutov"', sender="poad7498@gmail.com", recipients= login_session['recipientemail'] )
+        msg.body= request.form[awnser]
+        mail.send(msg)
+        return render_template('admin.html')
+    else:
+        try:
+            print (login_session['localId'])
+        except:
+            return redirect(url_for('awnser'))
+    UID = random.choice(list(db.child('questions').get().val()))
+    value=db.child('questions').get().val()[UID]
+    login_session['recipientemail']= value['email']
+    db.child('questions').child(UID).remove()
+    return render_template('admin.html', question=value)
 
-#Code goes above here
 
 if __name__ == '__main__':
     app.run(debug=True)
