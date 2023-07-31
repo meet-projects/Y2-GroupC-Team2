0d8c0b5a598b8e046510a6ef697423466b11beda
Yoav-Koursh <yoav.koursh@gmail.com>
2023-07-31 11:17:41 +0300

    work

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ auth= firebase.auth()
 
 arabictext=[]
 hebrewtext=[]
-englishtext=[]
+
 @app.route('/' , methods= ('POST', "get"))
 def about():
     if request.method=="POST":
@@ -69,18 +69,37 @@ def awnser():
         msg= Message('Awnser for question about out charity "shekulutov"', sender="poad7498@gmail.com", recipients= login_session['recipientemail'] )
         msg.body= request.form[awnser]
         mail.send(msg)
+        if request.form [popular]==True:
+            question_and_awnser=db.child('questions').child(UID).get().val()
+            question_and_awnser['awnser']= request.form['awnser']
+            db.child('popular').push(question_and_awnser )
+        db.child('questions').child(login_session['UID']).remove()
         return render_template('admin.html')
     else:
         try:
             print (login_session['localId'])
         except:
             return redirect(url_for('awnser'))
-    UID = random.choice(list(db.child('questions').get().val()))
-    value=db.child('questions').get().val()[UID]
-    login_session['recipientemail']= value['email']
-    db.child('questions').child(UID).remove()
-    return render_template('admin.html', question=value)
+    try:
+        UID = random.choice(list(db.child('questions').get().val()))
+        loggin_session['UID']= UID    
+        value=db.child('questions').get().val()[UID]
+        login_session['recipientemail']= value['email']
+        return render_template('admin.html', question=value)
+    except: 
+        return "all questions awnsered"
 
+@app.route("/signout")
+def signout():
+    auth.current_user=None
+    login_session['user']=None
+    return (redirect(url_for('about')))
+
+
+@app.route('/home', methods=['POST','GET'])
+@app.route('/home/<string:language>' ,methods=['POST', 'GET'])
+def home(language='he'):
+    return render_template('home.html', text= db.child('langueges').child(language).get().val())
 
 if __name__ == '__main__':
     app.run(debug=True)
